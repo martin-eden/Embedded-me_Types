e1214c2808405cf29b9693b496058ac5a0a3aa32
Ian <ian@somewhere.com>
2024-05-15 21:00:06 +0200

    [+] [example]

---
+++ examples/me_Types/me_Types.ino
@@ -0,0 +1,16 @@
+// [me_Types] example
+
+#include <me_Types.h>
+
+void setup()
+{
+  TUint_4 SerialSpeed = 57600;
+
+  Serial.begin(SerialSpeed);
+
+  Serial.println("[me_Types] Library reachable.");
+}
+
+void loop()
+{
+}
